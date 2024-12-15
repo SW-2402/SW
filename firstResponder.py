772b6f6e73ecdad25297143efe44d3149b9f90ad
Hyundol <58063456+Hyundol@users.noreply.github.com>
2024-12-15 21:06:39 +0900

    Emergency (#1)
    * ver2
    
    * GPS, dotenv, private

--- firstResponder.py
+++ firstResponder.py
@@ -1,13 +1,19 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+from firstResponderDB import FirstResponderDB
 
 class FirstResponder:
     def __init__(self):
-        self.name = None
-        self.phoneNumber = None
-
-    def sendNumber(self, ):
+        self.name = {}
+        self.phoneNumber = {}
+        self.db = FirstResponderDB()
+        
+    def __sendNumber(self, ):
+        self.db.addInfo("건구수", "010-1234-5678")
+        self.db.addInfo("김건국", "010-1111-2222")
+        FirstResponderinfo = self.db.getInfo()
+        self.name = list(FirstResponderinfo.keys())
+        self.phoneNumber = list(FirstResponderinfo.values())
         pass
 
     def getInfo(self, ):
-        pass
+        self.__sendNumber()
+        return self.name, self.phoneNumber
--- hospitalNumber.py
+++ hospitalNumber.py
@@ -1,12 +1,19 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+from hospitalAPI import HospitalAPI
+from gPSInfo import GPSInfo
 
 class HospitalNumber:
     def __init__(self):
         self.phoneNumber = None
-
-    def sendNumber(self, ):
+        self.hospitalAPI = HospitalAPI()
+        self.GPS = GPSInfo()
+        
+        
+    def __sendNumber(self, ):
+        longitude, latitude = self.GPS.getInfo()
+        _, self.phoneNumber = self.hospitalAPI.getInfo(longitude,latitude)
         pass
 
     def getInfo(self, ):
-        pass
+        self.__sendNumber()
+        return self.phoneNumber
+    
