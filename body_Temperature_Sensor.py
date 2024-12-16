fed1e2d700b026117335d2c0211c5288c20f1c9a
Glyph8 <72332160+Glyph8@users.noreply.github.com>
2024-12-16 21:05:09 +0900

    refactoring (#7)

--- body_Temperature_Sensor.py
+++ body_Temperature_Sensor.py
@@ -16,16 +16,3 @@ class Body_Temperature_Sensor(SensorInterface):
     def extractBioInfo(self):
         print("[Blood Pressure Sensor] Extracting blood pressure data...")
         self.data = generate_temperature_data(samples=1000)
-
-    # def __init__(self):
-    #     self.sensorInfo = "Body Temperature Sensor"
-    #     self.data = generate_temperature_data(samples=1000)
-    #
-    # def extractBioInfo(self):
-    #     """체온 데이터 반환"""
-    #     print("[Body Temperature Sensor] Extracting temperature data...")
-    #     return self.data
-    #
-    # def getBioInfo(self):
-    #     print("[Body Temperature Sensor] Returning sensor information...")
-    #     return self.sensorInfo
