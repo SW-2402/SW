3a137eaa4b7182fcbf30f79b120c6f19ba48863d
Hyundol <58063456+Hyundol@users.noreply.github.com>
2024-12-16 21:04:53 +0900

    add csv (#6)

--- firstResponder.py
+++ firstResponder.py
@@ -1,3 +1,4 @@
+import csv
 from firstResponderDB import FirstResponderDB
 
 class FirstResponder:
@@ -5,10 +6,24 @@ class FirstResponder:
         self.name = {}
         self.phoneNumber = {}
         self.db = FirstResponderDB()
+        self.__loadFromCSV("first_responder.csv")
         
+    def __loadFromCSV(self, file_path):
+        try:
+            with open(file_path, mode='r', encoding='utf-8') as file:
+                reader = csv.reader(file)
+                next(reader) 
+
+                for row in reader:
+                    if len(row) >= 2:
+                        name, phone_number = row[0], row[1]
+                        self.db.addInfo(name, phone_number)
+        except FileNotFoundError:
+            print(f"Error: 파일 '{file_path}'을(를) 찾을 수 없습니다.")
+        except Exception as e:
+            print(f"Error: CSV 파일 읽기 중 오류 발생 - {e}")
+    
     def __sendNumber(self, ):
-        self.db.addInfo("건구수", "010-1234-5678")
-        self.db.addInfo("김건국", "010-1111-2222")
         FirstResponderinfo = self.db.getInfo()
         self.name = list(FirstResponderinfo.keys())
         self.phoneNumber = list(FirstResponderinfo.values())
